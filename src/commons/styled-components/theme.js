412eb22404dee28712beee480abc642e664aabf7
Patrick Narciso <patrick.ncsilva@gmail.com>
2020-11-07 19:26:02 -0300

    feat(theme): add styled components with theme

---
+++ src/commons/styled-components/theme.js
@@ -0,0 +1,33 @@
+export default {
+	colors: {
+		primary: {
+			purple: {
+				100: '#6045AF',
+				200: '#3F2787',
+				800: '#1D1647',
+			},
+			green: {
+				100: '#65A300',
+			},
+		},
+		neutral: {
+			gray: {
+				20: '#F2F2F3',
+				200: '#8B8B92',
+				300: '#72737A',
+				400: '#595A63',
+				500: '#454550',
+				700: '#2B2B2B',
+				800: '#070817',
+			},
+		},
+	},
+	fonts: {
+		sizes: {
+			small: '14px',
+			medium: '16px',
+			large: '20px',
+		},
+		family: 'Lato',
+	},
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+
 import './index.css';
+import theme from './commons/styled-components/theme';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
