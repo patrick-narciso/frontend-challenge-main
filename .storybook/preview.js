56f6a2d15b4d0497dfb27dad89324b64984fa61f
Patrick Narciso <patrick.ncsilva@gmail.com>
2020-11-17 16:59:59 -0300

    docs(storybook): add custom font and theme on storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/commons/styled-components/theme';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [
+	(Story) => (
+		<ThemeProvider theme={theme}>
+			<Story />
+		</ThemeProvider>
+	),
+];
