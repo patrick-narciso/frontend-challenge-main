1a0c308a05823b75afa7c2a23b6dfbaff70b1e44
Patrick Narciso <patrick.ncsilva@gmail.com>
2020-11-18 15:37:53 -0300

    config: setup react router and removes unecessary default files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
-import './index.css';
 import theme, { GlobalStyle } from 'commons/styled-components/theme';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
